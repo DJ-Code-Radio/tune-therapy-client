f10bad18f5472ac4f14e3e933d5c8b0e3833a6d6
neginkou <negin.kou@gmail.com>
2023-12-18 16:33:07 -0800

    added image render function

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,67 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 function Quiz() {
-  const [books, setBooks] = useState([]);
+  const [images, setImages] = useState([]);
 
-  const handleBookCreate = async (newBook) => {
-    try {
-      let response = await axios.post(`${import.meta.env.VITE_SERVER}/books`, newBook);
-      setBooks([...books, response.data]);
-    } catch (error) {
-      console.error('Error creating book:', error);
-    }
-  };
+  const fetchImages = async () => {
+    // Dummy data
+    const dummyData = [
+      { id: 1, title: 'Image 1', url: 'src/assets/pexels-roman-odintsov-6898855.jpg' },
+      { id: 2, title: 'Image 2', url: 'src/assets/pexels-roman-odintsov-6898855.jpg' },
+      // Add more dummy data as needed
+    ];
+    // const handleBookCreate = async (newBook) => {
+    //     try {
+    //       let response = await axios.post(`${import.meta.env.VITE_SERVER}/books`, newBook);
+    //       setBooks([...books, response.data]);
+    //     } catch (error) {
+    //       console.error('Error creating book:', error);
+    //     }
+    //   };
 
-  const handleDelete = async (e) => {
     try {
-      let response = await axios.delete(`${import.meta.env.VITE_SERVER}/books/${e.target.id}`);
-      let book = response.data;
-      let newBooks = books.filter((book) => {
-        return book.title !== e.target.id;
-      });
-      setBooks(newBooks);
+      // Simulate API call by setting the dummy data directly
+      setImages(dummyData);
     } catch (error) {
-      console.error('Error deleting book:', error);
-    }
-  };
-
-  const fetchBooks = async () => {
-    try {
-      console.log(import.meta.env.VITE_SERVER);
-      const response = await axios.get(`${import.meta.env.VITE_SERVER}/books`);
-      console.log('Here are the server books', response);
-      setBooks(response.data);
-    } catch (error) {
-      console.error('Error fetching books:', error);
-    }
-  };
-
-  const handleBookUpdate = async (book) => {
-    try {
-      console.log('Sending updated book to server', book);
-      let response = await axios.put(`${import.meta.env.VITE_SERVER}/books/${book._id}`, book);
-      let updatedBook = response.data;
-      console.log('From the server, the updated book is', updatedBook);
-      let newBooksList = books.map((existingBook) =>
-        existingBook._id === updatedBook._id ? updatedBook : existingBook
-      );
-      setBooks(newBooksList);
-    } catch (error) {
-      console.error('Error updating book:', error);
+      console.error('Error fetching images:', error);
     }
   };
 
   useEffect(() => {
-    fetchBooks();
+    fetchImages();
   }, []);
 
   return (
-    <>
-      <p>Hello world</p>
-          
-    </>
+    <div>
+      <h2>Image Gallery</h2>
+      <ul>
+        {images.map((image) => (
+          <li key={image.id}>
+            <img src={image.url }width='150px' alt={image.title} />
+            <p>{image.title}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
+
 
 export default Quiz;
