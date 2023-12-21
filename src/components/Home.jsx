65b04846bca2d0991afee6dbb2d4534d8ee47cec
Errol Vidad <errolvidad@gmail.com>
2023-12-21 02:28:09 +0000

    added auth0 button to play button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,9 +2,19 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Image from 'react-bootstrap/Image';
+import { useAuth0 } from '@auth0/auth0-react'; // Import the Auth0 hook
 import '../css/Home.css';
 
 function Home() {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  // Function to handle login
+  const handleLogin = () => {
+    if (!isAuthenticated) {
+      loginWithRedirect();
+    }
+  };
+
   return (
     <Row>
       <Col className="container">
@@ -15,7 +25,14 @@ function Home() {
         </ListGroup>
       </Col>
       <Col className="container image-col">
-        <Image src="src/assets/play-30619_1280.png" width='175px' roundedCircle />
+        {/* Add a button or clickable element that triggers the Auth0 login */}
+        <Image
+          src="src/assets/play-30619_1280.png"
+          width='175px'
+          roundedCircle
+          onClick={handleLogin} // Call handleLogin when the image is clicked
+          style={{ cursor: 'pointer' }} // Add a pointer cursor to indicate clickability
+        />
       </Col>
     </Row>
   );
