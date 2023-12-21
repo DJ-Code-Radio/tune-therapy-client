32eaf810671515f2f0149ace420a1bb7074e8128
Errol Vidad <errolvidad@gmail.com>
2023-12-21 23:02:31 +0000

    try 3

--- src/auth/AuthButtons.jsx
+++ src/auth/AuthButtons.jsx
@@ -1,19 +1,13 @@
-import { useAuth0} from '@auth0/auth0-react';
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import Login from "./Login";
 import Logout from "./Logout";
 
-
-
-
 function AuthButtons() {
-  
 
   const {
     isAuthenticated,
-   
   } = useAuth0();
-
-  
 
   return isAuthenticated ? <Logout /> : <Login />
 }
