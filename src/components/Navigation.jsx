cbca1b0fb39a10a5dd6903e3f075f63826d50579
Errol Vidad <errolvidad@gmail.com>
2023-12-22 22:17:14 +0000

    updated href in navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -7,9 +7,9 @@ function Navigation(){
     return (
         <Navbar  expand="lg" className="bg-body-tertiary" fixed="bottom" style={{display:'flex', flexDirection: 'row', justifyContent: 'center', zIndex: '1', backgroundClip: 'text'}}>
             <Nav className="me">
-              <Nav.Link style={{margin: "5px", fontSize: "25px", color:"white", fontFamily:'fantasy'}} href="">Home</Nav.Link>
-              <Nav.Link style={{margin: "5px", fontSize: "25px", color:"white", fontFamily:'fantasy'}} href="quiz">Quiz</Nav.Link>
-              <Nav.Link style={{margin: "5px", fontSize: "25px", color:"white", fontFamily:'fantasy'}} href="about">About</Nav.Link>
+              <Nav.Link style={{margin: "5px", fontSize: "25px", color:"white", fontFamily:'fantasy'}} href="/">Home</Nav.Link>
+              <Nav.Link style={{margin: "5px", fontSize: "25px", color:"white", fontFamily:'fantasy'}} href="/quiz">Quiz</Nav.Link>
+              <Nav.Link style={{margin: "5px", fontSize: "25px", color:"white", fontFamily:'fantasy'}} href="/about">About</Nav.Link>
             </Nav>
       
       </Navbar>
